bff407572459f85fed39da89c434030ba8465ed2
juanrevello <juanmanuelrevello@gmail.com>
2021-03-07 09:33:58 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import random
+
+guessesTaken = 0
+minNumber = 1
+maxNumber = 20
+
+print("Hello! What is your name?: ")
+username = input()
+
+number = random.randint(minNumber, maxNumber)
+print("Well, " + username + '. I am thinking in a number between ' + str(minNumber) + ' and ' + str(maxNumber))
+
+while guessesTaken < 6:
+    print("Take a guess: ")
+    guess = input()
+    guess = int(guess)
+
+    guessesTaken = guessesTaken + 1
+
+    if guess < number:
+        print("Your guess is too Low.")
+    if guess > number:
+        print("Your guess is too High.")
+    if guess == number:
+        break
+
+if guess == number:
+    guessesTaken = str(guessesTaken)
+    print("Good Job. " + username + "! you guessed my number in " + guessesTaken + ' guesses')
+
+if guess != number:
+    number = str(number)
+    print("No, the number I was thinking of was " + number)
